bb51bfb9982bfeda2fce193c5cd64566b2760e48
Moisés Mariano <moisesduartem@gmail.com>
2022-02-06 17:13:12 -0300

    simple express server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,5 @@
-import express from 'express';
+import express from "express";
 
-const app = express()   ;
+const app = express();
 
-function x(k) {
-    return;
-
-    console.log('x')
-}
+app.listen(3333, () => console.log("Server is running!"));
